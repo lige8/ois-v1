972568a19265c632c5f6465c9f261c09c3d9e050
lige8 <0abcdefghijklmnopqrstuvwxyz27@gmail.com>
2021-02-28 19:14:16 +0100

    aaaaa

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,9 +1,21 @@
 public class Gravitacija{
 
      public static void main(String []args){
-        System.out.println("OIS je zakon!");
-     }
-        public static void izpis (double podanaVisina, double gravitacijskiPospesek){
+
+    Scanner sc = new Scanner(System.in);
+    double nadmorskaVisina = sc.nextInt();
+    double gravitacijskiPospesek = gravitacijskiPospesek(nadmorskaVisina);
+    izpis(nadmorskaVisina,gravitacijskiPospesek);
+    }
+         public static double gravitacijskiPospesek (double nadmorskaVisina){
+             double konstanta = 6.674 * Math.pow(10, -11);
+             double masaZemlje = 5.972 * Math.pow(10, 24);
+             double polmerZemlje = 6.371 * Math.pow(10, 6);
+             double gravitacijskiPospesek = (konstanta * masaZemlje) / (Math.pow((polmerZemlje + nadmorskaVisina), 2));
+             return gravitacijskiPospesek;
+      }
+
+         public static void izpis (double podanaVisina, double gravitacijskiPospesek){
               System.out.println(podanaVisina);
               System.out.println(gravitacijskiPospesek);
 
