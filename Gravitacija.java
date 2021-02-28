7c22b5643ce03853145e87664160d561a06963f7
lige8 <0abcdefghijklmnopqrstuvwxyz27@gmail.com>
2021-02-28 18:33:37 +0100

    Dodajanje izračuna pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,4 +3,10 @@ public class Gravitacija{
      public static void main(String []args){
         System.out.println("OIS je zakon!");
      }
+      public static double gravitacijskiPospesek (double nadmorskaVisina){
+             double konstanta = 6.674 * Math.pow(10, -11);
+             double masaZemlje = 5.972 * Math.pow(10, 24);
+             double polmerZemlje = 6.371 * Math.pow(10, 6);
+             double gravitacijskiPospesek = (konstanta * masaZemlje) / (Math.pow((polmerZemlje + nadmorskaVisina), 2));
+             return gravitacijskiPospesek;
 }
